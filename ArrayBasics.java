890e0d2b7537385fd37379bbf0ed59491768fd4c
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-08-22 23:20:45 +0530

    more patterns and solved arrays

---
+++ ArrayBasics.java
@@ -0,0 +1,171 @@
+public class ArrayBasics {
+    
+    public static void update(int marks[]){
+
+        for(int i=0;i<marks.length;i++){
+            marks[i]+=1;
+        }
+    }
+  
+     public static int linearSearch(int marks[],int key){
+
+          for(int i=0;i<marks.length;i++){
+
+            if(marks[i]==key){
+                return i;
+            }
+          }
+            
+          return -1;
+     }
+
+     public static int linearSearch(String marks[],String key){
+
+
+        for(int i=0;i<marks.length;i++){
+
+            if(marks[i]==key){
+                return i;
+            }
+        }
+            return -1;
+
+     }
+
+     public static int largest(int arr[]){
+
+        int largest=Integer.MIN_VALUE;
+        int smallest=Integer.MAX_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+
+            if(largest<arr[i]){
+                largest=arr[i];
+            }
+
+            if(smallest>arr[i]){
+                smallest=arr[i];
+            }
+        }
+        System.out.println("Smallest value:"+smallest);
+        return largest;
+
+     }
+
+     public static int binarySearch(int arr[],int key){
+
+        //works only on sorted array
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+
+          int mid=(start+end)/2;
+
+          if(arr[mid]==key){
+            return mid;
+          }
+
+          else if(arr[mid]<key){
+            start=mid+1;
+          }
+
+          else{
+            end=mid-1;
+          }
+
+        }
+
+        return -1;
+
+     }
+
+     public static void reverse(int arr[]){
+
+
+         int start=0;
+         int end=arr.length-1;
+
+         while(start<end){
+
+            int temp=arr[end];
+            arr[end]=arr[start];
+            arr[start]=temp;
+
+            start++;
+            end--;
+
+         }
+
+     }
+
+     public static void pairs(int arr[]){
+
+        for(int i=0;i<arr.length-1;i++){
+
+            for(int j=i+1;j<arr.length;j++){
+
+                System.out.print("("+arr[i]+","+arr[j]+"), ");
+            }
+            System.out.println();
+        }
+     }
+
+     public static void subarray(int arr[]){
+
+
+     }
+
+    public static void main(String args[]){
+
+
+        //initializing an array
+//   int marks[]= new int[50];
+//   System.out.println(marks[0]);
+
+
+//   int marks[]={98,99,100};
+// update(marks);
+
+// System.out.println(marks[0]);
+
+// //linear search
+// int marlks[]={98,99,89,98,67,88,90};
+// int key=90;
+// System.out.println(linearSearch(marlks, key));
+
+// String menu[]={"samosa","daal","idli","dosa"};
+// String key="dosa";
+// System.out.println(linearSearch(menu, key));
+
+//largest fo 2 numbers in an array
+
+// int marks[]={1,2,34,5,6,3,4,2,9,6,10};
+
+// System.out.println(largest(marks));
+
+
+//binary search
+// int marks[]={2,4,6,8,10,12,14};
+// int key=10;
+// System.out.println(binarySearch(marks, key));
+
+//void reverse
+//  int marks[]={2,4,6,8,10,12,14};
+//  reverse(marks);
+
+
+//pairs in an array
+// int arr[]={2,4,6,8,10};
+// pairs(arr);
+
+
+//subarray
+int arr[]={2,4,6,8,10};
+subarray(arr);
+
+
+
+
+    }
+}
--- JavaBasics2.java
+++ JavaBasics2.java
@@ -351,6 +351,91 @@ public class JavaBasics2 {
         }
 
     }
+
+    public static void solidRhombus(int n){
+
+ 
+         for(int i=1;i<=n;i++){
+              
+             for(int j=1;j<=n-i;j++){
+
+                System.out.print("  ");
+             }
+
+             for(int k=1;k<=n;k++){
+                System.out.print("* ");
+             }
+
+             System.out.println();
+         }
+
+    }
+
+    public static void hollowSolidRhombus(int n){
+
+
+        for(int i=1;i<=n;i++){
+              
+            for(int j=1;j<=n-i;j++){
+
+               System.out.print("  ");
+            }
+
+            for(int k=1;k<=n;k++){
+
+                if((i==1 || i==n) || (k==1 || k==n)){
+                      System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+    public static void diamond(int n){
+
+        if(n%2!=0){
+            n+=1;
+        }
+
+        int half=n/2;
+
+        for(int i=1;i<=half;i++){
+
+            for(int j=1;j<=half-i;j++){
+                System.out.print("  ");
+            }
+
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+       //for other half of diamond
+        for(int i=1;i<=half;i++){
+            
+        
+            for(int j=1;j<=i-1;j++){
+
+                System.out.print("  ");
+            }
+
+
+            for(int k=1;k<=n+1-2*i;k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+        }
+    }
     public static void main(String args[]){
 
 
@@ -444,9 +529,16 @@ public class JavaBasics2 {
         // triangle(10);
 
         //butterfly
-        butterfly(7);
+        // butterfly(7);
+
+       //solidrhombus
+    //    solidRhombus(5);
 
 
+    //hollow solid rhoumbus
+    // hollowSolidRhombus(25);
 
+    //diamond
+    diamond(11);
     }
 }
