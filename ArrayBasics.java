e18bb9e15e8b0f5990ab23128694ca55aef6bba7
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-08-22 23:22:10 +0530

    solved arrays

--- ArrayBasics.java
+++ ArrayBasics.java
@@ -113,6 +113,18 @@ public class ArrayBasics {
 
      public static void subarray(int arr[]){
 
+         for(int i=0;i<arr.length;i++){
+
+             for(int j=i;j<arr.length;j++){
+
+                 for(int k=i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                 }
+                 
+             }
+
+             System.out.println();
+         }
 
      }
 
