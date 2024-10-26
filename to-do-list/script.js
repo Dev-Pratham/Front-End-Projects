ef3d4ebf3af5bb307dcede25d277838462b2a8bb
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-10-27 00:33:26 +0530

    further modified my ui of todo app and added functionality read about js dom

---
+++ to-do-list/script.js
@@ -0,0 +1,13 @@
+
+const buttn=document.querySelector(".main button");
+const scrhButton=document.querySelector(".main input");
+
+let html="<ul><li>";
+buttn.addEventListener("click",()=>{
+
+    let list=scrhButton.value;
+    // console.log(document.cookie);
+    console.log(document.implementation); // Logs the HTML element
+
+
+})
