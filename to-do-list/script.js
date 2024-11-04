2bf1e7fd125137a7c1a5a276c429ca228697326e
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-11-05 00:26:27 +0530

    resolved some bugs in the web app

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,13 +1,49 @@
 
-const buttn=document.querySelector(".main button");
-const scrhButton=document.querySelector(".main input");
 
-let html="<ul><li>";
-buttn.addEventListener("click",()=>{
+const inputBox=document.getElementById("text");
+const searchBox=document.getElementById("bttn");
+const refList=document.getElementById("listitems");
 
-    let list=scrhButton.value;
-    // console.log(document.cookie);
-    console.log(document.implementation); // Logs the HTML element
+
+
+searchBox.addEventListener("click",myFunc1);
+
+function myFunc1(){
+
+ if(inputBox.value===''){
+    alert("Please enter something");
+ }
+
+ else{
+    const li=document.createElement("li");
+    li.innerHTML=inputBox.value;
+   //  refList.appendChild(li);
+
+    const span=document.createElement("span");
+    span.innerHTML='\u00D7';
+    li.appendChild(span);
+    refList.appendChild(li);
+
+ }
+
+ inputBox.value='';
+
+}
+
+listitems.addEventListener("click",function(e){
+
+
+   if(e.target.tagName === "LI"){
+      e.target.classList.toggle("checked");
+   }
+
+   else if(e.target.tagName==="SPAN"){
+      e.target.parentElement.remove();
+   }
+
 
 
 })
+
+
+
